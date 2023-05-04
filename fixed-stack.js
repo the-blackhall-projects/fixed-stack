de12ca24bc8cf6eb0dc2c2d03a316a5c23bf0f6f
michael-megan <sba21652@student.cct.ie>
2023-05-04 22:14:16 +0100

    Return null instead of undefined in tail method

--- fixed-stack.js
+++ fixed-stack.js
@@ -1,5 +1,35 @@
-class FixedStack {
-	construcor(lenght) {
-		this.lenght = lenght;
+
+/**
+ * Fixed length circular buffer.  Once full, tail returns the oldest
+ * object to be inserted into the buffer.  Before buffer is full
+ * tail returns an undefined quantity.
+ */
+class Buffer {
+
+
+	constructor(length) {
+		this.head = 0;
+		this.length = length;
+		this.data = Array.apply(null, Array(length)).map(function () {});
+	}
+
+	insert(obj) {
+		this.data[this.head] = obj;
+		++this.head;
+		this.head = this.head % this.length;
+	}
+
+	get tail() {
+		let pointer = (this.head+this.length) % this.length;
+
+		let retVal = this.data[pointer];
+
+		if (typeof retVal === "undefined") {
+			return null;
+		} else {
+			return retVal;
+		}
 	}
 }
+
+
